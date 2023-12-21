795b4769315c686d8be20eb82eba6101b6204ca6
Вячеслав Приказов <vaceslavprikazov@s-MacBook-Air.local>
2023-12-21 16:53:56 +0300

    second commit

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import "fmt"
 
+func number() {
+	var a, b, y int
+	var operation string
+
+	fmt.Scan(&a, &operation, &b)
+	if operation == "+" {
+		y = a + b
+		fmt.Print(y)
+	} else if operation == "-" {
+		y = a - b
+		fmt.Print(y)
+	} else if operation == "*" {
+		y = a * b
+		fmt.Print(y)
+	} else if operation == "/" && b != 0 {
+		y = a / b
+		fmt.Print(y)
+	} else if b == 0 {
+		fmt.Print("На ноль делить нельзя")
+	}
+
+}
 func main() {
-	var hello string
-	hello = "hello"
-	fmt.Println(hello)
-	fmt.Println("hello")
+	number()
 }
